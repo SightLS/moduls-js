4304645ca298db3bdb7c095d4a9b7b7362e61625
sighLS <sightlesssf46@gmail.com>
2022-08-26 20:59:55 +0400

    работа счетсчика в консоли

--- timeer/main.js
+++ timeer/main.js
@@ -8,20 +8,18 @@ const sec = document.querySelector('.seconds__block');
 const btnStrat = document.querySelector('.start');
 const btnStop = document.querySelector('.stop')
 
-
+let test = 60
 function counter(){
   const interval = setInterval(()=>{
-    console.log(sec)
-  }, 1000)
-  // callback()
+    test = --test;
+    console.log(test)
+  }, 500)
+  btnStop.addEventListener('click', e =>{
+    clearInterval(interval)
+    console.log(test)
+  })
+
 }
-//
-// const stop = ()=>{
-//   return clearInterval(interval)
-// }
 btnStrat.addEventListener('click', e =>{
   counter();
-})
-btnStop.addEventListener('click', e =>{
-  // counter(stop) 
 })
