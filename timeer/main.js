d4fbe07ec031be3c9988c30ef9f10b13291d1560
sighLS <sightlesssf46@gmail.com>
2022-08-26 19:10:09 +0400

    не успел, позже доделаю

---
+++ timeer/main.js
@@ -0,0 +1,27 @@
+import timer from "./timer.js";
+timer()
+
+
+document.querySelector('.hours__block');
+document.querySelector('.minutes__block');
+const sec = document.querySelector('.seconds__block');
+const btnStrat = document.querySelector('.start');
+const btnStop = document.querySelector('.stop')
+
+
+function counter(callback){
+  const interval = setInterval(()=>{
+    console.log(sec)
+  }, 1000)
+  callback()
+}
+
+const stop = ()=>{
+  return clearInterval(interval)
+}
+btnStrat.addEventListener('click', e =>{
+  counter();
+})
+btnStop.addEventListener('click', e =>{
+  counter(stop)
+})
---
+++ timeer/timer.js
@@ -0,0 +1,69 @@
+
+function timer(){
+  class Timer{
+    constructor(timer, time, maxTime){
+      this.timer = timer;
+      this.time = time;
+      this.maxTime = maxTime;
+    }
+    render(){
+      const kek =  `
+         <div class="${this.time}">
+            <p class="${this.time}__block">${this.timer}</p>
+            <button class="${this.time}__up up">up</button>
+            <button class="${this.time}__down down">down</button>
+        </div>
+    `
+      document.querySelector('.timer').insertAdjacentHTML('beforeend', kek);
+    }
+    clickUp(){
+      const btnUp = document.querySelector(`.${this.time}__up`);
+      const btnDown = document.querySelector(`.${this.time}__down`)
+
+      btnUp.addEventListener('click', (e) =>{
+        if(this.timer < this.maxTime){
+          this.timer = this.timer + 1
+          document.querySelector(`.${this.time}__block`).innerText = this.timer
+        }
+
+
+      })
+      btnDown.addEventListener('click', (e) =>{
+        if(this.timer > 0){
+          this.timer = this.timer - 1
+          document.querySelector(`.${this.time}__block`).innerText = this.timer
+        }
+
+      })
+    }
+  }
+
+
+
+
+  const hours = new Timer(0, 'hours', 24)
+  const minutes = new Timer(0, 'minutes', 60)
+  const seconds = new Timer(0, 'seconds', 60)
+  hours.render();
+  minutes.render();
+  seconds.render();
+  hours.clickUp();
+  minutes.clickUp();
+  seconds.clickUp();
+
+
+  const btnStart = '<button class="start">старт!</button>'
+  const btnstop = '<button class="stop">stop!</button>'
+
+  document.querySelector('.timer').insertAdjacentHTML('beforeend', btnStart);
+  document.querySelector('.timer').insertAdjacentHTML('beforeend', btnstop);
+
+}
+
+
+
+export default timer;
+
+
+
+
