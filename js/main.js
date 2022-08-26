5941d3d21db74594051941c62967490c63c80b94
sighLS <sightlesssf46@gmail.com>
2022-08-27 00:53:04 +0400

    доделал дз 2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // import { DateTime } from './luxon.js'
 import { printError, printResult } from './printResult.js'
 import getDateDiff from './getDateDiff.js'
+import timer from "./timer.js";
 
 const form = document.getElementById("datecalc")
 
@@ -21,3 +22,22 @@ form.onsubmit = (event) => {
     printResult(dateDiff)
   }
 }
+timer()
+const openCalcBtn = document.querySelector('.calc-open');
+const openTimerBtn = document.querySelector('.timer-open');
+
+openTimerBtn.addEventListener('click', e=>{
+  let timerMenu = document.querySelector('.timer');
+  timerMenu.style.display = 'inline';
+  let calcMenu = document.querySelector('form');
+  calcMenu.style.display = 'none';
+
+
+})
+openCalcBtn.addEventListener('click', e=>{
+  let timerMenu = document.querySelector('.timer');
+  timerMenu.style.display = 'none';
+  let calcMenu = document.querySelector('form');
+  calcMenu.style.display = 'inline';
+
+})
